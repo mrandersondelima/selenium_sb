b718370a8c2b9e7ccf2ca1f8266c3f513d88efeb
Anderson Lima <mrandersondelima@gmail.com>
2025-02-12 16:58:55 -0300

    mensagem de procurando jogos

--- app.py
+++ app.py
@@ -1537,7 +1537,15 @@ Aposta {self.qt_true_bets_made}""")
                         first_match_to_start = first_match_to_start - timedelta(hours=3)
 
                         if first_match_to_start - datetime.now() > timedelta(minutes=6):
-                            print(f'procurando jogos até { self.formata_data( jogos_aptos[0]['original_start_date'], "%Y-%m-%dT%H:%M:%SZ" ) }')
+                            horario_jogo = self.formata_data( jogos_aptos[0]['original_start_date'], "%Y-%m-%dT%H:%M:%SZ" )
+                            print(f'procurando jogos até { horario_jogo }')
+                            if first_match_to_start != self.first_match_to_start_date:
+                                self.first_match_to_start_date = first_match_to_start
+                                self.escreve_em_arquivo('first_match_to_start_date.txt', self.first_match_to_start_date, 'w')
+                                try:
+                                    await self.telegram_bot.envia_mensagem(f'Procurando jogos até { horario_jogo }')
+                                except Exception as e:
+                                    print(e)
                             await self.wait_for_next_fixture_search(datetime.now() + timedelta(minutes=5))
                             continue
 
